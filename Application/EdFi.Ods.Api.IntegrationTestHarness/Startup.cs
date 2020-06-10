6488eab04aed1275b9493130b65981554a82e056
Jim McKay <jmckay@certicasolutions.com>
2020-06-09 19:35:25 -0500

    Sprint 30 rebase changes

--- Application/EdFi.Ods.Api.IntegrationTestHarness/Startup.cs
+++ Application/EdFi.Ods.Api.IntegrationTestHarness/Startup.cs
@@ -2,13 +2,16 @@
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
- 
+
 using EdFi.Ods.Api.IntegrationTestHarness;
 using EdFi.Ods.Api.Startup;
 using EdFi.Ods.ChangeQueries;
 using EdFi.Ods.Common.Extensions;
 using EdFi.Ods.Composites.Test;
+using EdFi.Ods.Features;
 using EdFi.Ods.Profiles.Test;
+using EdFi.Ods.Security;
+using EdFi.Ods.Standard;
 using Microsoft.Owin;
 
 [assembly: OwinStartup("Startup", typeof(Startup))]
@@ -22,6 +25,10 @@ namespace EdFi.Ods.Api.IntegrationTestHarness
             AssemblyLoader.EnsureLoaded<Marker_EdFi_Ods_Composites_Test>();
             AssemblyLoader.EnsureLoaded<Marker_EdFi_Ods_Profiles_Test>();
             AssemblyLoader.EnsureLoaded<Marker_EdFi_Ods_ChangeQueries>();
+            AssemblyLoader.EnsureLoaded<Marker_EdFi_Ods_Security>();
+            AssemblyLoader.EnsureLoaded<Marker_EdFi_Ods_Standard>();
+            AssemblyLoader.EnsureLoaded<Marker_EdFi_Ods_Api>();
+            AssemblyLoader.EnsureLoaded<Marker_EdFi_Ods_Features>();
         }
     }
 }
