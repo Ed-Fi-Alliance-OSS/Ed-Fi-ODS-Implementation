782075aa8e00fb4bff3922f94cf588b032195a8b
Thomas Nunnery <t.nunnery@wearedoubleline.com>
2020-06-15 13:20:19 -0500

    Updated NuGet package versions for Postgres' populated and minimal templates.

--- DatabaseTemplate/Scripts/PostgreSqlPopulatedTemplate.ps1
+++ DatabaseTemplate/Scripts/PostgreSqlPopulatedTemplate.ps1
@@ -5,7 +5,7 @@
 
 $params = @{
     packageName = "EdFi.Ods.Populated.Template.PostgreSQL"
-    packageVersion = "4.0.0-b10143"
+    packageVersion = "4.0.0-b10149"
     packageSource = "https://www.myget.org/F/ed-fi/"
 }
 
