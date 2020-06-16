4af7fc6e0faf3f608ccc92fcea1f4c8f3afc53d0
Thomas Nunnery <t.nunnery@wearedoubleline.com>
2020-06-16 14:36:15 -0500

    [ODS-4239] Bring in TPDM model changes from TPDMDEV-188 and TPDMDEV-207 (#19)
    * Bringing in TPDM changes.
    
    * Updated NuGet package versions for populated template database and minimal template database.
    
    * Updated NuGet package versions for Postgres' populated and minimal templates.
    
    * Added sample data for tpdm extension.
    
    * Removed sample data for TPDM.

--- DatabaseTemplate/Scripts/PostgreSqlMinimalTemplate.ps1
+++ DatabaseTemplate/Scripts/PostgreSqlMinimalTemplate.ps1
@@ -5,7 +5,7 @@
 
 $params = @{
     packageName = "EdFi.Ods.Minimal.Template.PostgreSQL"
-    packageVersion = "4.0.0-b10145"
+    packageVersion = "4.0.0-b10151"
     packageSource = "https://www.myget.org/F/ed-fi/"
 }
 
