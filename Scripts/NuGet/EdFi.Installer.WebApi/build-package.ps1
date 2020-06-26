f053450348ac36171adaec25902ae72bb65c99a6
Stephen A. Fuqua <stephen@safnet.com>
2020-06-26 16:03:15 -0500

    Adjust a few file locations

--- Scripts/NuGet/EdFi.Installer.WebApi/build-package.ps1
+++ Scripts/NuGet/EdFi.Installer.WebApi/build-package.ps1
@@ -27,7 +27,7 @@ param (
 )
 $ErrorActionPreference = "Stop"
 
-Invoke-Expression "$PSScriptRoot/../prep-installer-package.ps1 $PSScriptRoot"
+Invoke-Expression "$PSScriptRoot/../shared/prep-installer-package.ps1 $PSScriptRoot"
 
 $verbose = $PSCmdlet.MyInvocation.BoundParameters["Verbose"]
 
