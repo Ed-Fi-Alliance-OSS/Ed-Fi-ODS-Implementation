d3fba7c146faf42f99d533528d6014e2c88da153
Thomas Nunnery <t.nunnery@wearedoubleline.com>
2020-07-02 11:11:22 -0500

    Updated populated template version.

--- DatabaseTemplate/Scripts/GrandBend.ps1
+++ DatabaseTemplate/Scripts/GrandBend.ps1
@@ -5,7 +5,7 @@
 
 $params = @{
     packageName    = "EdFi.Ods.Populated.Template.Suite3"
-    packageVersion = "4.0.0-b10187"
+    packageVersion = "4.0.0-b10188"
     packageSource  = "https://www.myget.org/F/ed-fi/"
 }
 
