8de1eaeef6e3d9792d91f1973d4492df09efcb75
Jim McKay <jmckay@certicasolutions.com>
2020-07-17 17:01:11 -0500

    updated background jobs for postgres

--- Application/EdFi.Ods.SandboxAdmin.Web/App_Start/BackgroundJobsConfig.cs
+++ Application/EdFi.Ods.SandboxAdmin.Web/App_Start/BackgroundJobsConfig.cs
@@ -2,38 +2,55 @@
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
- 
+
 using System;
 using System.Linq;
 using EdFi.Ods.Admin.Initialization;
+using EdFi.Ods.Common.Configuration;
 using Hangfire;
 
 namespace EdFi.Ods.SandboxAdmin.Web
 {
-    public static class BackgroundJobsConfig
+    public class BackgroundJobsConfig
     {
+        private readonly InitializationEngine _engine;
+        private readonly InitializationModel _initializationModel;
+        private readonly IApiConfigurationProvider _apiConfigurationProvider;
+
+        public BackgroundJobsConfig(InitializationEngine engine, InitializationModel initializationModel, IApiConfigurationProvider apiConfigurationProvider)
+        {
+            _engine = engine;
+            _initializationModel = initializationModel;
+            _apiConfigurationProvider = apiConfigurationProvider;
+        }
+
         /// <summary>
         /// Create background jobs to populate users and sandboxes
         /// This class separates the initialization engine from HangFire
         /// </summary>
-        public static void Configure(InitializationEngine engine, InitializationModel initializationModel)
+        public void Configure()
         {
-            if (!initializationModel.Enabled)
+            if (!_initializationModel.Enabled)
             {
                 return;
             }
 
-            // initial creation of roles, users, and sandboxes at server startup
-            var id1 = BackgroundJob.Enqueue(() => engine.CreateRoles());
-            var id2 = BackgroundJob.ContinueJobWith(id1, () => engine.CreateUsers());
-            var id3 = BackgroundJob.ContinueJobWith(id2, () => engine.EnsureMinimalTemplateEducationOrganizationsExist());
-            BackgroundJob.ContinueJobWith(id3, () => engine.CreateSandboxes());
+            if (_apiConfigurationProvider.DatabaseEngine == DatabaseEngine.SqlServer)
+            {
+                // initial creation of roles, users, and sandboxes at server startup for sql server only
+                var id1 = BackgroundJob.Enqueue(() => _engine.CreateRoles());
+                BackgroundJob.ContinueJobWith(id1, () => _engine.CreateUsers());
+            }
+
+            // add vendors and sandboxes
+            var id2 = BackgroundJob.Enqueue(() => _engine.CreateVendors());
+            BackgroundJob.ContinueJobWith(id2, () => _engine.CreateSandboxes());
 
             // refresh existing sandboxes periodically
-            if (initializationModel.Users.Any(u => u.Sandboxes.Any(s => s.Refresh)))
+            if (_initializationModel.Users.Any(u => u.Sandboxes.Any(s => s.Refresh)))
             {
                 // Change the recurrence to suit your needs using Cron functions or a unix CRON expressions (i.e. "* */6 * * *" = every 6 hours)
-                RecurringJob.AddOrUpdate("RebuildSandboxes", () => engine.RebuildSandboxes(), Cron.Daily(), TimeZoneInfo.Local);
+                RecurringJob.AddOrUpdate("RebuildSandboxes", () => _engine.RebuildSandboxes(), Cron.Daily(), TimeZoneInfo.Local);
             }
         }
     }
