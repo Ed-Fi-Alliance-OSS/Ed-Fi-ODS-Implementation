73a864b76e4de405ad2258c2655caa2cec65267a
Raghu Ramakrishna <rramakrishna@inniveinc.com>
2020-07-28 19:34:44 +0530

    [ODS-4341] Added header in Startup class

--- Application/EdFi.Ods.WebApi.NetCore/Startup.cs
+++ Application/EdFi.Ods.WebApi.NetCore/Startup.cs
@@ -1,8 +1,13 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
 using Microsoft.AspNetCore.Hosting;
 
 namespace EdFi.Ods.WebApi.NetCore
 {
-    public class Startup : EdFi.Ods.Api.NetCore.Startup.StartupBase
+    public class Startup : EdFi.Ods.Api.NetCore.Startup.OdsStartupBase
     {
         public Startup(IWebHostEnvironment env) : base(env)
         {
