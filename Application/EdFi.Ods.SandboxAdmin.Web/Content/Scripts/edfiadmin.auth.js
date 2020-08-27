ae31c6dae9cd39b43e168cd8b585a4d5f8da49ac
Benjamin Meyers <blmeyers@users.noreply.github.com>
2020-08-27 17:09:32 -0400

    [ODS-4403] Reverting unintentional formatting changes (#96)

--- Application/EdFi.Ods.SandboxAdmin.Web/Content/Scripts/edfiadmin.auth.js
+++ Application/EdFi.Ods.SandboxAdmin.Web/Content/Scripts/edfiadmin.auth.js
@@ -6,6 +6,7 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+
 var EdFiAdmin = EdFiAdmin || {};
 EdFiAdmin.Auth = {};
 
