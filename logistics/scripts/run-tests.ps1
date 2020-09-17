e7b1e23e6b459f2a878eb086c8e0c64cef36cfea
Jim McKay <jamessmckay@users.noreply.github.com>
2020-09-17 11:39:20 -0500

    [ODS-4540] Update Builds to Correctly Handle dotnet test Failures (#115)

---
+++ logistics/scripts/run-tests.ps1
@@ -0,0 +1,23 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
+& "$PSScriptRoot\modules\load-path-resolver.ps1"
+
+$testAssemblies = (Get-ChildItem -recurse -File $((Get-RepositoryRoot "ed-fi-ods") + "\*Tests.dll") | Where-Object { $_.FullName -match "\\bin\\?" -and $_.FullName -notmatch "\\net48\\?" -and $_.fullName -notmatch "ApprovalTests.dll" })
+$reports = (Get-RepositoryRoot "ed-fi-ods-implementation") + "\reports\"
+
+if (Test-Path $reports) {
+    Remove-Item -Path $reports -Force -Recurse
+}
+
+New-Item -ItemType Directory -Force -Path $reports
+
+foreach ($assembly in $testAssemblies) {
+    Write-Host ( "Testing assembly " + $assembly)
+
+    $reportName = $reports + (Get-ChildItem $assembly | Select-Object -ExpandProperty Name) + ".xml"
+
+    & dotnet test $assembly --logger ("trx;LogFileName=" + $reportName)
+}
