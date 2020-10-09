fcb16eb89f4845199b94fc0914a3131b6c2c0eb9
semalaiappan <34613894+semalaiappan@users.noreply.github.com>
2020-10-09 11:18:24 -0500

    Update Application/EdFi.Ods.Api.IntegrationTestHarness/Startup.cs
    Co-authored-by: Jim McKay <jamessmckay@users.noreply.github.com>

--- Application/EdFi.Ods.Api.IntegrationTestHarness/Startup.cs
+++ Application/EdFi.Ods.Api.IntegrationTestHarness/Startup.cs
@@ -3,9 +3,6 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 using EdFi.Ods.Api.Startup;
-using EdFi.Ods.Common.Extensions;
-using EdFi.Ods.Composites.Test;
-using EdFi.Ods.Profiles.Test;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 
