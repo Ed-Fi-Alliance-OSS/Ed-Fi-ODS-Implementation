5b0075c9f6f0ff3a47bab4d6f51b5bc92491eca8
Thomas Nunnery <t.nunnery@wearedoubleline.com>
2020-10-16 12:02:13 -0500

    Included TPDM package reference

--- Plugin/development.ps1
+++ Plugin/development.ps1
@@ -11,7 +11,7 @@ $pluginPaths = @()
 
 $parameters = @{
     packageName     = "EdFi.Suite3.Ods.Extensions.Sample"
-    packageVersion  = "5.1.0-b10399"
+    packageVersion  = "5.1.0-b10403"
     packageSource   = "https://www.myget.org/F/ed-fi/"
     outputDirectory = "$PSScriptRoot"
     toolsPath       = (Get-ToolsPath)
@@ -20,7 +20,16 @@ $pluginPaths += Get-NuGetPackage @parameters
 
 $parameters = @{
     packageName     = "EdFi.Suite3.Ods.Extensions.Homograph"
-    packageVersion  = "5.1.0-b10412"
+    packageVersion  = "5.1.0-b10418"
+    packageSource   = "https://www.myget.org/F/ed-fi/"
+    outputDirectory = "$PSScriptRoot"
+    toolsPath       = (Get-ToolsPath)
+}
+$pluginPaths += Get-NuGetPackage @parameters
+
+$parameters = @{
+    packageName     = "EdFi.Suite3.Ods.Extensions.TPDM"
+    packageVersion  = "5.1.0-b10401"
     packageSource   = "https://www.myget.org/F/ed-fi/"
     outputDirectory = "$PSScriptRoot"
     toolsPath       = (Get-ToolsPath)
