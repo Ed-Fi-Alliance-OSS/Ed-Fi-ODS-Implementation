73f79240dd894aaee3d9e62195fb60b016a13fb6
Geoff McElhanon <geoffrey@mcelhanon.com>
2020-11-04 11:30:13 -0600

    [ODS-4592] Duplicates from Security Views (#171)

--- DatabaseTemplate/Scripts/EdFiMinimalTemplate.ps1
+++ DatabaseTemplate/Scripts/EdFiMinimalTemplate.ps1
@@ -5,7 +5,7 @@
 
 $params = @{
     packageName    = "EdFi.Suite3.Ods.Minimal.Template"
-    packageVersion = "5.1.0-b10358"
+    packageVersion = "5.1.0-b10370"
     packageSource  = "https://www.myget.org/F/ed-fi/"
 }
 
