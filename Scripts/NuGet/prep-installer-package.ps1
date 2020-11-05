68b9d8e14319d584de0922781341ec0895827e11
Travis Meador <t.meador@wearedoubleline.com>
2020-11-05 12:08:26 -0600

    [ODS-4595] Add Hashtable Module to AppCommon (#175)

--- Scripts/NuGet/prep-installer-package.ps1
+++ Scripts/NuGet/prep-installer-package.ps1
@@ -15,7 +15,7 @@ $ErrorActionPreference = "Stop"
 Push-Location $PackageDirectory
 
 $dependencyVersions = @{
-    AppCommon = "1.1.0-pre1127"
+    AppCommon = "1.1.0"
 }
 
 $edFiRepoContainer = "$PackageDirectory/../../../.."
