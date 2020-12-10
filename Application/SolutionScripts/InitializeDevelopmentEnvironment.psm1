8970c86475c0526d8b2f6a24acffbdb38de17e5e
Thomas Nunnery <t.nunnery@wearedoubleline.com>
2020-12-09 21:08:10 -0600

    Changed code gen package version.

--- Application/SolutionScripts/InitializeDevelopmentEnvironment.psm1
+++ Application/SolutionScripts/InitializeDevelopmentEnvironment.psm1
@@ -9,7 +9,7 @@ $ErrorActionPreference = 'Stop'
 
 $toolVersion = @{
     dbDeploy = "2.2.0-b10049"
-    codeGen = "5.2.0-b11308"
+    codeGen = "5.2.0-b11310"
 }
 
 & "$PSScriptRoot\..\..\logistics\scripts\modules\load-path-resolver.ps1"
