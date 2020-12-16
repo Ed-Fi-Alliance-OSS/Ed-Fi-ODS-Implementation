5740e9621f2287ba147db4402437cdafed197e94
Raghu Ramakrishna <rramakrishna@inniveinc.com>
2020-12-16 21:58:21 +0530

    added UseReverseProxyHeaders flag in the configuration

--- Application/EdFi.Ods.SandboxAdmin/Extensions/RequestExtensions.cs
+++ Application/EdFi.Ods.SandboxAdmin/Extensions/RequestExtensions.cs
@@ -18,9 +18,8 @@ namespace EdFi.Ods.Sandbox.Admin.Extensions
         private const string XForwardedHost = "X-Forwarded-Host";
         private const string XForwardedPort = "X-Forwarded-Port";
 
-        public static string ToAbsolutePath(this HttpRequest request, string relativePath = "~", string hashFragment = null)
+        public static string ToAbsolutePath(this HttpRequest request, string relativePath = "~", string hashFragment = null, bool useProxyHeaders = false)
         {
-            bool useProxyHeaders = UseProxyHeaders(request);
             string path;
             string query;
 
