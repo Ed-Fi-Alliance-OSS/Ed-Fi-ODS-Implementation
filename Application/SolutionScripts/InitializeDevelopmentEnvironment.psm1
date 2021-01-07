6c0a0c546d85e6a94d53b53769b57e3ff74945f9
semalaiappan <34613894+semalaiappan@users.noreply.github.com>
2021-01-06 20:00:53 -0600

    Update InitializeDevelopmentEnvironment.psm1

--- Application/SolutionScripts/InitializeDevelopmentEnvironment.psm1
+++ Application/SolutionScripts/InitializeDevelopmentEnvironment.psm1
@@ -414,7 +414,7 @@ function Invoke-CodeGen {
                 else
                 {
                     $extensionLocationPlugins += $extensionPath + ','
-                }   
+                }
                 $counter++
             }
         }
