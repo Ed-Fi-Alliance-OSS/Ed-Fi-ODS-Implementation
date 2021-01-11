bad12134af083a1d83934ada42c8b8702ad533da
Jim McKay <jmckay@certicasolutions.com>
2021-01-11 11:39:16 -0600

    fixed script for reporting of tests correctly

--- logistics/scripts/run-tests.ps1
+++ logistics/scripts/run-tests.ps1
@@ -3,10 +3,10 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
-& "$PSScriptRoot\modules\load-path-resolver.ps1"
+& "$PSScriptRoot/modules/load-path-resolver.ps1"
 
 $solutionPath = (Get-RepositoryResolvedPath "Application/Ed-Fi-Ods.sln")
-$reports = (Get-RepositoryRoot "ed-fi-ods-implementation") + "\reports\"
+$reports = (Get-RepositoryRoot "ed-fi-ods-implementation") + "/reports/"
 
 if (Test-Path $reports) {
     Remove-Item -Path $reports -Force -Recurse
@@ -16,8 +16,6 @@ New-Item -ItemType Directory -Force -Path $reports
 
 Write-Host "Testing Solution at $solutionPath"
 
-$reportName = $reports + "Ed-Fi-Ods-Tests.xml"
+Write-Host -ForegroundColor Magenta "& dotnet test $solutionPath --no-build --no-restore --results-directory $reports --logger 'trx'"
 
-Write-Host -ForegroundColor Magenta "& dotnet test $solutionPath --no-build --no-restore --logger (""trx;LogFileName=$reportName"")"
-
-& dotnet test $solutionPath --no-build --no-restore --logger ("trx;LogFileName=$reportName")
+& dotnet test $solutionPath --no-build --no-restore --results-directory $reports --logger "trx"
