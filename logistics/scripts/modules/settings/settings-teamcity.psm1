75ba89d5941e1164efc686444831853cc957e58e
travis.meador <t.meador@wearedoubleline.com>
2021-01-14 15:50:56 -0600

    add license header

--- logistics/scripts/modules/settings/settings-teamcity.psm1
+++ logistics/scripts/modules/settings/settings-teamcity.psm1
@@ -1,3 +1,8 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
 & "$PSScriptRoot\..\..\..\..\logistics\scripts\modules\load-path-resolver.ps1"
 Import-Module -Force -Scope Global (Get-RepositoryResolvedPath "logistics\scripts\modules\tasks\TaskHelper.psm1")
 Import-Module -Force -Scope Global (Get-RepositoryResolvedPath 'logistics\scripts\modules\utility\hashtable.psm1')
