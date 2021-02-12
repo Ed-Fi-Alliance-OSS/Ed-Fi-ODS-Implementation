c76195f2e15a03f2b2eb22bf7edd32ea03e97681
Jim McKay <jamessmckay@users.noreply.github.com>
2021-02-12 14:06:37 -0600

    [ODS-4612] Optional References in ODS /API as directed by ed-fi model (#224)

--- Application/SolutionScripts/InitializeDevelopmentEnvironment.psm1
+++ Application/SolutionScripts/InitializeDevelopmentEnvironment.psm1
@@ -9,7 +9,7 @@ $ErrorActionPreference = 'Stop'
 
 $toolVersion = @{
     dbDeploy = "2.2.0-b10049"
-    codeGen  = "5.2.0-b11505"
+    codeGen  = "5.2.0-b11513"
 }
 
 & "$PSScriptRoot/../../logistics/scripts/modules/load-path-resolver.ps1"
