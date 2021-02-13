4a516685034942eea3695f28c220b55d9a7cf092
semalai <m.semalaiappan@gmail.com>
2021-02-13 15:19:53 -0600

    updated Codegen

--- Application/SolutionScripts/InitializeDevelopmentEnvironment.psm1
+++ Application/SolutionScripts/InitializeDevelopmentEnvironment.psm1
@@ -9,7 +9,7 @@ $ErrorActionPreference = 'Stop'
 
 $toolVersion = @{
     dbDeploy = "2.2.0-b10049"
-    codeGen  = "5.2.0-b11478"
+    codeGen  = "5.2.0-b11527"
 }
 
 & "$PSScriptRoot/../../logistics/scripts/modules/load-path-resolver.ps1"
