189fcff69af64efe05d02f5228f74e26d0d33d28
semalai <m.semalaiappan@gmail.com>
2021-04-22 09:50:12 -0500

    removed writehost

--- logistics/scripts/modules/TestHarness.psm1
+++ logistics/scripts/modules/TestHarness.psm1
@@ -71,7 +71,7 @@ function Invoke-SdkGenConsole {
     $sdkGenConsoleFolder = (Get-RepositoryResolvedPath "/Utilities/SdkGen/EdFi.SdkGen.Console")
     $sdkGenConsoleExecutableFolder = Join-Path -Path $sdkGenConsoleFolder -ChildPath "/bin/$buildConfiguration/netcoreapp3.1"    
     $sdkGenConsoleFullPath = (Join-Path $sdkGenConsoleExecutableFolder 'EdFi.SdkGen.Console')
-    & $sdkGenConsoleFullPath -p $true -c $true -i $true -m $apiMetadataUrl -v $sdkCliVersion | Write-Host
+    & $sdkGenConsoleFullPath -p $true -c $true -i $true -m $apiMetadataUrl -v $sdkCliVersion
 }
 
 function Start-TestHarness {
