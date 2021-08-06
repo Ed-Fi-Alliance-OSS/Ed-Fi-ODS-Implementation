0f69dcd714856aa6978aa3fbc58f99dbcc2f1575
raghuramakrishna <68504753+raghuramakrishna@users.noreply.github.com>
2021-08-06 20:19:39 +0530

    [ODS-4772] Unable to load Glendale PostgreSQL database via Initdev (#304)

--- Application/SolutionScripts/InitializeDevelopmentEnvironment.psm1
+++ Application/SolutionScripts/InitializeDevelopmentEnvironment.psm1
@@ -370,6 +370,7 @@ function Reset-TestPopulatedTemplateDatabase {
             subTypeNames            = Get-DefaultSubtypes
             dropDatabase            = $true
             createByRestoringBackup = $backupPath
+            databaseTimeoutInSeconds = $settings.ApiSettings.PopulatedTemplateDBTimeOutInSeconds
         }
 
         Initialize-EdFiDatabaseWithDbDeploy @params
