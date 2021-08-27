74d0ba4aea07507fad5a14563b3956f64a940546
Benjamin Meyers <blmeyers@users.noreply.github.com>
2021-08-27 18:06:52 -0400

    Expanding get-populated-from-web to better handle non zip/7z file types, such as directly downloading a bak file.

--- DatabaseTemplate/Modules/get-populated-from-web.ps1
+++ DatabaseTemplate/Modules/get-populated-from-web.ps1
@@ -5,9 +5,9 @@
 
 <#
 .SYNOPSIS
-    Downloads a zip archive containing the populated template backup
+    Downloads a backup file or zip archive containing the template backup
 .DESCRIPTION
-    Given a source url to a zip archive this will download and unzip to the Database folder.
+    Given a source url to a backup file or zip archive this will download and extract if necessary to the Database folder.
 .INPUTS
     [uri] Source Url
     [string] File Name
@@ -15,7 +15,7 @@
     None.
 #>
 param (
-    # The source url to download the zip archive from
+    # The source url to download the backup from
     [Parameter(Mandatory)]
     [uri] $sourceUrl,
     # The name of the file that is downloaded from the source url
@@ -23,31 +23,49 @@ param (
     [string] $fileName
 )
 
-if (-not (Get-InstalledModule | Where-Object -Property Name -eq "7Zip4Powershell")) {
-    Install-Module -Force -Scope CurrentUser -Name 7Zip4Powershell
+$isArchiveFile = $fileName.EndsWith('.zip') -or $fileName.EndsWith('.7z')
+$archiveBackupFilePath = Join-Path $global:templateFolder $fileName
+$finalBackupFilePath = Join-Path $global:templateDatabaseFolder $fileName
+
+if (Test-Path -Path $global:templateDatabaseFolder\*) {
+    Write-Host "Template found, using existing file at $global:templateDatabaseFolder"
+    return
 }
+
+if (Test-Path $global:templateDatabaseFolder) { Remove-Item -Force -Recurse -Path $global:templateDatabaseFolder | Out-Null }
+New-Item -Path $global:templateDatabaseFolder -ItemType "Directory" | Out-Null
 
 & "$PSScriptRoot\..\..\logistics\scripts\modules\load-path-resolver.ps1"
 Import-Module -Force -Scope Global (Get-RepositoryResolvedPath "DatabaseTemplate\Modules\database-template-source.psm1")
 
 # using WebClient is faster then Invoke-WebRequest but shows no progress
-Write-host "Downloading file from $sourceUrl..."
+Write-Host "Downloading file from $sourceUrl..."
 $webClient = New-Object System.Net.WebClient
-$webClient.DownloadFile($sourceUrl, "$global:templateFolder\$fileName")
+if ($isArchiveFile) {
+    $webClient.DownloadFile($sourceUrl, $archiveBackupFilePath)
 
-Write-host "Download complete."
+    if (-not (Test-Path $archiveBackupFilePath)) {
+        Write-Error "Template source file '$archiveBackupFilePath' not found."
+        exit -1
+    }
+}
+else {
+    $webClient.DownloadFile($sourceUrl, $finalBackupFilePath)
 
-if (Test-Path $global:templateDatabaseFolder) { Remove-Item -Force -Recurse -Path $global:templateDatabaseFolder | Out-Null }
-New-Item -Path $global:templateDatabaseFolder -ItemType "Directory" | Out-Null
-
-if (-not (Test-Path "$global:templateFolder\$fileName")) {
-    Write-Warning "Populated Template source file '$fileName' not found."
-    exit 0
+    if (-not (Test-Path $finalBackupFilePath)) {
+        Write-Error "Template source file '$finalBackupFilePath' not found."
+        exit -1
+    }
 }
 
-if ($fileName.EndsWith('.zip') -or $fileName.EndsWith('.7z')) {
-    Write-host "Extracting $fileName..."
-    Expand-7Zip -ArchiveFileName "$global:templateFolder\$fileName" -TargetPath "$global:templateDatabaseFolder"
-    Write-Host "Extracted to: $global:templateDatabaseFolder\$fileName" -ForegroundColor Green
+Write-Host "Download complete."
+
+if ($isArchiveFile) {
+    if (-not (Get-InstalledModule | Where-Object -Property Name -eq "7Zip4Powershell")) {
+        Install-Module -Force -Scope CurrentUser -Name 7Zip4Powershell
+    }
+    Write-Host "Extracting $fileName..."
+    Expand-7Zip -ArchiveFileName $archiveBackupFilePath -TargetPath $global:templateDatabaseFolder
+    Write-Host "Extracted to: $global:templateDatabaseFolder" -ForegroundColor Green
 }
 if ($error.count -gt 0 -or $LASTEXITCODE -gt 0) { exit 1; }
