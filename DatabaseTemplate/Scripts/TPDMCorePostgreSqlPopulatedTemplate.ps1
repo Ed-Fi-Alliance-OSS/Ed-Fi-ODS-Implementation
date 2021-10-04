2ea1591a79f39f64ad1958064ba76377a1fe9646
travis.meador <t.meador@wearedoubleline.com>
2021-10-04 14:11:07 -0500

    add template source scripts

---
+++ DatabaseTemplate/Scripts/TPDMCorePostgreSqlPopulatedTemplate.ps1
@@ -0,0 +1,24 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
+#requires -modules "path-resolver"
+
+Import-Module -Force -Scope Global (Get-RepositoryResolvedPath 'logistics\scripts\modules\packaging\nuget-helper.psm1')
+Import-Module -Force -Scope Global (Get-RepositoryResolvedPath "logistics\scripts\modules\tools\ToolsHelper.psm1")
+Import-Module -Force -Scope Global (Get-RepositoryResolvedPath 'logistics\scripts\modules\utility\hashtable.psm1')
+
+$configurationFile = (Get-RepositoryResolvedPath 'logistics/scripts/configuration.packages.json')
+$configuration = (Get-Content $configurationFile | ConvertFrom-Json).packages.TPDMCorePostgreSqlPopulatedTemplate
+
+$parameters = @{
+    packageName     = $configuration.packageName
+    packageVersion  = $configuration.packageVersion
+    packageSource   = $configuration.packageSource
+    outputDirectory = "$PSScriptRoot/../Database"
+    toolsPath       = (Get-ToolsPath)
+}
+$packagePath = Get-NuGetPackage @parameters
+
+return Get-ChildItem "$packagePath/*.bak"
