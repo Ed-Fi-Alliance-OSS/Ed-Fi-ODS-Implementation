45644c804c66baad9ee382ea259e023fd3fe96f3
Rosh Dhanawade <rdhanawa@indiana.edu>
2021-11-04 12:00:13 -0400

    Update 0002-EducationOrganizations.sql
    Added Comment

--- Application/EdFi.Ods.Extensions.NMPED/Artifacts/MsSql/Data/Ods/0002-EducationOrganizations.sql
+++ Application/EdFi.Ods.Extensions.NMPED/Artifacts/MsSql/Data/Ods/0002-EducationOrganizations.sql
@@ -2,7 +2,7 @@ DECLARE @LocalEducationAgencyCategoryDescriptorId int;
 SELECT @LocalEducationAgencyCategoryDescriptorId=DescriptorId
 from edfi.Descriptor where Namespace like '%LocalEducationAgencyCategoryDescriptor%' and CodeValue = 'Independent';
 
-SELECT * FROM edfi.Descriptor where Namespace like '%LocalEducationAgencyCategoryDescriptor%' 
+--SELECT * FROM edfi.Descriptor where Namespace like '%LocalEducationAgencyCategoryDescriptor%' 
 
 INSERT INTO edfi.EducationOrganization (EducationOrganizationId, NameOfInstitution) 
 								  VALUES (11,'Doug');
