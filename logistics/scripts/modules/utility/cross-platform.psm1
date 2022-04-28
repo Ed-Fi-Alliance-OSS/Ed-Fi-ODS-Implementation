f8f731bc8dbcdbae1055d572019667fcb11d04af
Jordan Rogers <j.rogers@wearedoubleline.com>
2022-04-28 10:08:12 -0500

    initial new module commit

---
+++ logistics/scripts/modules/utility/cross-platform.psm1
@@ -0,0 +1,15 @@
+function GetIsWindows {
+    <#
+    .SYNOPSIS
+        Checks to see if the current machine is a Windows machine.
+    
+    .EXAMPLE
+        GetIsWindows
+
+        returns $True
+    #>
+
+
+}
+
+Export-ModuleMember -Function "GetIsWindows"
