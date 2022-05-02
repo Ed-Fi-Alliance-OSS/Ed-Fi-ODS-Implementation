552a3cff7fa75ee17a538530a70283031d9fc346
Jordan Rogers <j.rogers@wearedoubleline.com>
2022-05-02 10:06:44 -0500

    commit to save both methods

--- logistics/scripts/modules/utility/cross-platform.psm1
+++ logistics/scripts/modules/utility/cross-platform.psm1
@@ -1,15 +1,39 @@
-function GetIsWindows {
+function Get-IsWindows {
     <#
     .SYNOPSIS
         Checks to see if the current machine is a Windows machine.
     
     .EXAMPLE
-        GetIsWindows
+        Get-IsWindows
 
         returns $True
     #>
 
+    # Method 1
+    # This could honestly just be the first if statement and nothing else
+    if (!$IsWindows) {
+        $IsUnix = $false
+        $IsWindows = $true
+    }elseif($IsWindows -eq $false){
+        $IsUnix = $true
+    }elseif($IsWindows -eq $true){
+        $IsUnix = $false
+    }
 
+    # Method 2
+    <#
+    $psVersion = $PSVersionTable
+    if ($psVersion.Platform -eq "Unix") {
+        $IsUnix = $true
+    }elseif (!$psVersion.Platform) {
+        $IsUnix = $false
+    }elseif($psVersion.Platform -like "*Win*"){
+        $IsUnix = $false
+    }
+    #>
+
+
+    return $IsWindows
 }
 
-Export-ModuleMember -Function "GetIsWindows"
+Export-ModuleMember -Function "Get-IsWindows"
