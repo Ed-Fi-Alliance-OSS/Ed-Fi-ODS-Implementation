d5f359f4a20af80178133743fc9309402f3d091b
Jordan Rogers <j.rogers@wearedoubleline.com>
2022-05-02 10:09:04 -0500

    only check if IsWindows exists

--- logistics/scripts/modules/utility/cross-platform.psm1
+++ logistics/scripts/modules/utility/cross-platform.psm1
@@ -9,29 +9,10 @@ function Get-IsWindows {
         returns $True
     #>
 
-    # Method 1
-    # This could honestly just be the first if statement and nothing else
     if (!$IsWindows) {
         $IsUnix = $false
         $IsWindows = $true
-    }elseif($IsWindows -eq $false){
-        $IsUnix = $true
-    }elseif($IsWindows -eq $true){
-        $IsUnix = $false
     }
-
-    # Method 2
-    <#
-    $psVersion = $PSVersionTable
-    if ($psVersion.Platform -eq "Unix") {
-        $IsUnix = $true
-    }elseif (!$psVersion.Platform) {
-        $IsUnix = $false
-    }elseif($psVersion.Platform -like "*Win*"){
-        $IsUnix = $false
-    }
-    #>
-
 
     return $IsWindows
 }
