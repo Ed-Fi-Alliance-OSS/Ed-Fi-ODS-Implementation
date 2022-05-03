09dce024872f770327295161c230e6245e6e914e
Jordan Rogers <j.rogers@wearedoubleline.com>
2022-05-03 13:49:29 -0500

    add comments to intentionally errored line

--- logistics/scripts/modules/utility/cross-platform.psm1
+++ logistics/scripts/modules/utility/cross-platform.psm1
@@ -10,6 +10,9 @@ function Get-IsWindows {
     #>
 
     if ($null -eq $IsWindows) {
+        # This section will only trigger when the automatic $IsWindows variable is not detected.
+        # Every version of PS released on Linux contains this variable so it will always exist.
+        # $IsWindows does not exist pre PS 6, so this will create it in that case.
         $IsWindows = $true
     }
 
