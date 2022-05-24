76dd7c32202ec99fbb9f72da5577302d89a18746
Eric Stockdale <ericstockdale@gmail.com>
2022-05-24 15:10:30 -0700

    Comment regarding overwriting appsettings.json when creating templates

--- DatabaseTemplate/Modules/create-database-template.psm1
+++ DatabaseTemplate/Modules/create-database-template.psm1
@@ -28,11 +28,11 @@ function Get-DefaultTemplateConfiguration([hashtable] $config = @{ }) {
     $config = Merge-Hashtables $config, (Get-EnvironmentConfiguration $config)
     $config.outputFolder = (Get-ChildItem "$(Get-RepositoryResolvedPath "Application\EdFi.Ods.Api.IntegrationTestHarness")\bin\**\*").FullName
     
-	# Since not all features are enabled by default for database templates, an appsettings.json specifically intended for database generation is copied to
-	# the Integration Test Harness output folder to overwrite the appsettings.json file otherwise used by the Integration Test Harness
-	$config.appSettingsFolder = (Get-RepositoryResolvedPath "DatabaseTemplate/Modules/")
+    # Since not all features are enabled by default for database templates, an appsettings.json specifically intended for database generation is copied to
+    # the Integration Test Harness output folder to overwrite the appsettings.json file otherwise used by the Integration Test Harness
+    $config.appSettingsFolder = (Get-RepositoryResolvedPath "DatabaseTemplate/Modules/")
 	
-	$sourceAppSettings = Join-Path $config.appSettingsFolder "appsettings.json"
+    $sourceAppSettings = Join-Path $config.appSettingsFolder "appsettings.json"
     $destinationAppSettings = Join-Path $config.outputFolder "appsettings.json"
     Copy-Item $sourceAppSettings -Destination $destinationAppSettings
 	
