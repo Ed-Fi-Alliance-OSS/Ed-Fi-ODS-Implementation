0050e9fb2eefc03ec55e0ca24d7eb2186c0d5041
semalai <m.semalaiappan@gmail.com>
2022-05-25 11:24:27 -0500

    $repositoryRoot updated

--- Application/SolutionScripts/InitializeDevelopmentEnvironment.psm1
+++ Application/SolutionScripts/InitializeDevelopmentEnvironment.psm1
@@ -372,7 +372,7 @@ function Invoke-CodeGen {
         $codeGen = (Join-Path $toolsPath 'EdFi.Ods.CodeGen')
         $repositoryRoot = (Get-RepositoryRoot $implementationRepo).Replace($implementationRepo, '')
 
-        Write-Host  '$RepositoryRoot' -ForegroundColor Magenta $RepositoryRoot
+        Write-Host  '$RepositoryRoot' -ForegroundColor Magenta $repositoryRoot
 
         $parameters = @(
             "-r", $repositoryRoot,
