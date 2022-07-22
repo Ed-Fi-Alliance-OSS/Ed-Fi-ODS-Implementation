33f323ca19af997bf16a6b9805c3fa6f21edfc67
semalai <m.semalaiappan@gmail.com>
2022-07-21 23:58:45 -0500

    Clean up code

--- logistics/scripts/modules/packaging/promotion.psm1
+++ logistics/scripts/modules/packaging/promotion.psm1
@@ -129,8 +129,6 @@ Function Invoke-PromotePackages {
 
     if ($PSCmdlet.ShouldProcess($PackagesURL)) {
         $response = Invoke-WebRequest @parameters
-        ##$response = Invoke-RestMethod -Uri "$PackagesURL/nuget/packagesBatch?api-version=5.0-preview.1" -Headers @{Authorization = $Password}   -ContentType "application/json" -Method POST -Body (ConvertTo-Json $body -Depth 10)     
-        
         $response | ConvertTo-Json -Depth 10 | Out-Host
     }
 
