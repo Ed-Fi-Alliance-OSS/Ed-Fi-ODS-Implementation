b3909507b47e9fb6ab5f602334c7f076a035bc05
semalai <m.semalaiappan@gmail.com>
2022-07-31 22:55:43 -0500

    Removed Debugging code

--- logistics/scripts/modules/database/database-management.psm1
+++ logistics/scripts/modules/database/database-management.psm1
@@ -383,7 +383,6 @@ Function Get-Server {
     if ($username) {
         # Initialize Server instance using standard security
         # Write-Host "Connecting to SQL Server using standard security for user '$username'..."
-        Write-Host "Connecting to SQL Server using standard security for user '$username'..."
 
         $s = New-Object ('Microsoft.SqlServer.Management.Smo.Server') $sql_server
         $s.ConnectionContext.LoginSecure = $false
@@ -393,7 +392,6 @@ Function Get-Server {
     else {
         # Initialize Server instance using integrated security
         # Write-Host "Connecting to SQL Server using integrated security..."
-        Write-Host "Connecting to SQL Server using integrated security..."     
         $s = New-Object ('Microsoft.SqlServer.Management.Smo.Server') $sql_server
     }
 
