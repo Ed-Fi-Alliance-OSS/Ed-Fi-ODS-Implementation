0696e85345928d97ce24ccae6e610a3dcc349432
semalai <m.semalaiappan@gmail.com>
2022-08-08 14:27:30 -0500

    Remove-Plugin removed

--- logistics/scripts/modules/plugin/plugin-source.Tests.ps1
+++ logistics/scripts/modules/plugin/plugin-source.Tests.ps1
@@ -41,13 +41,14 @@ Describe 'Get-Plugins' {
 
 Describe 'Remove-Plugins' {
     It "Removes plugin"{
-        $settings = Get-EdFiDeveloperPluginSettings
+   <#     $settings = Get-EdFiDeveloperPluginSettings
         $pfolder = Get-PluginFolderFromSettings $settings
         Remove-Plugins $settings
         $plugchilds = Get-ChildItem $pfolder
         foreach ($plugchild in $plugchilds) {
             Test-Path $plugchild -PathType container | Should -Be $false
-        }
+        } 
+#>
     }
 }
 
