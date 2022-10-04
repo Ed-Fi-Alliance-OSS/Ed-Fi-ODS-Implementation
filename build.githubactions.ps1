77428921223e41f3f03a62ed79835b7a5ef93c31
Eric Stockdale <ericstockdale@gmail.com>
2022-10-03 22:02:13 -0700

    nuget pack for minimal template action

--- build.githubactions.ps1
+++ build.githubactions.ps1
@@ -56,9 +56,6 @@ param(
     $NuspecFile,
     
     [string]
-    $OutputDirectory,
-    
-    [string]
     $Authors,
     
     [string]
@@ -136,7 +133,7 @@ function Compile {
 function Pack {
     if ($NuspecFile){
         Invoke-Execute {
-            nuget pack $NuspecFile -Version $InformationalVersion -OutputDirectory "$OutputDirectory" -p id="$PackageName" -p authors="$Authors" -p description="$Description"
+            nuget pack $NuspecFile -Version $version -OutputDirectory "$packagePath" -p id="$PackageName" -p authors="$Authors" -p description="$Description"
         }
         return
     } 
