79d6c9466124a18d5589a8cc19788a4819c50956
Ben Tillman <ben.tillman@accenture.com>
2022-10-07 17:21:20 -0500

    Extract out step into powershell

--- build.githubactions.ps1
+++ build.githubactions.ps1
@@ -7,7 +7,7 @@
 param(
     # Command to execute, defaults to "Build".
     [string]
-    [ValidateSet("Clean", "Build", "Test", "Pack", "Publish")]
+    [ValidateSet("Clean", "Build", "Test", "Pack", "Publish", "CheckoutBranch")]
     $Command = "Build",
 
     [switch] $SelfContained,
@@ -52,7 +52,10 @@ param(
     $TestFilter,
 
     [string]
-    $NuspecFilePath
+    $NuspecFilePath,
+
+    [string]
+    $RepositoryDispatchBranch
 )
 
 $newRevision = ([int]$BuildCounter) + ([int]$BuildIncrementer)
@@ -168,6 +171,41 @@ function Test {
     }
 }
 
+function CheckoutBranch {
+    $odsBranch = $Env:REPOSITORY_DISPATCH_BRANCH
+    Write-Output "OdsBranch: $odsBranch"
+    if($odsBranch -ne ''){
+        $patternName = "refs/heads/$odsBranch"
+        $does_corresponding_branch_exist = $false
+        $does_corresponding_branch_exist = git ls-remote --heads origin $odsBranch | Select-String -Pattern $patternName -SimpleMatch -Quiet
+        if ($does_corresponding_branch_exist -eq $true) {
+            Write-Output "Corresponding branch for $odsBranch exists in Implementation repo, so checking it out"
+            git fetch origin $odsBranch
+            git checkout $odsBranch
+        } else {
+            WriteHost "Corresponding branch for $odsBranch does not exist in Implementation repo, so not changing branch checked out"
+        }
+    } else {
+        Write-Output "ref_name: $Env:REF_NAME"
+        $current_branch = $Env:REF_NAME
+        if ($current_branch -like "*/merge"){
+            Write-Output "ref_name is PR, so using head_ref: $Env:HEAD_REF"
+            $current_branch = $Env:HEAD_REF
+        }
+        $patternName = "refs/heads/$current_branch"
+        Write-Output "Pattern Name is " $patternName -fore GREEN
+        $branch_exists = $false
+        $branch_exists = git ls-remote --heads origin $current_branch | Select-String -Pattern $patternName -SimpleMatch -Quiet
+        if ($branch_exists -eq $true) {
+            Write-Output "Current branch exists, so setting to $current_branch"
+            git fetch origin $current_branch
+            git checkout $current_branch
+        } else {
+            Write-Output "did not match on any results for changing ODS checkout branch"
+        }
+    }
+}
+
 function Invoke-Build {
     Write-Host "Building Version $version" -ForegroundColor Cyan
     Invoke-Step { Clean }
@@ -186,6 +224,10 @@ function Invoke-Pack {
     Invoke-Step { Pack }
 }
 
+function Invoke-CheckoutBranch {
+    Invoke-Step { CheckoutBranch }
+}
+
 Invoke-Main {
     switch ($Command) {
         Clean { Invoke-Clean }
@@ -193,6 +235,7 @@ Invoke-Main {
         Test { Invoke-Tests }
         Pack { Invoke-Pack }
         Publish { Invoke-Publish }
+        CheckoutBranch { Invoke-CheckoutBranch }
         default { throw "Command '$Command' is not recognized" }
     }
 }
