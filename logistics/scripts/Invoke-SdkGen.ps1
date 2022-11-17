810f68b26e88d6eb76da2cfc5172061321b9ec1e
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2022-11-17 10:39:31 -0600

    Removed profiles option from SDK generation.

--- logistics/scripts/Invoke-SdkGen.ps1
+++ logistics/scripts/Invoke-SdkGen.ps1
@@ -84,7 +84,7 @@ function Invoke-SdkGen {
                 $script:result += Invoke-Task "Invoke-RebuildSolution" { Invoke-RebuildSolution $buildConfiguration "minimal"  $sdkGenSolution }
                 $script:result += Invoke-Task -name "Start-TestHarness" -task { Start-TestHarness $apiUrl $configurationFile $environmentFilePath $null "EdFiOdsApiSdk" }
                 $sdkCliVersion = Get-ValueOrDefault $teamCityParameters['SdkCliVersion'] '6.0.1'
-                $arguments = @("-v",$sdkCliVersion,"-p","-c","-i")
+                $arguments = @("-v",$sdkCliVersion,"-c","-i")
                 $script:result += Invoke-Task "Invoke-SdkGenConsole" { Invoke-SdkGenConsole $apiMetadataUrl $buildConfiguration $arguments }
             }
             finally {
