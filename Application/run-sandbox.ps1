b0061826eb99d6db15f0dae8903922e1638813c9
Stephen A. Fuqua <stephen@safnet.com>
2022-12-15 15:39:22 -0600

    Simple script for starting WebApi, Swagger, and Sandbox Admin from the command line

---
+++ Application/run-sandbox.ps1
@@ -0,0 +1,20 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
+$env:ASPNETCORE_ENVIRONMENT="Development"
+
+Start-Process powershell -WorkingDirectory $PSScriptRoot -ArgumentList {
+    &dotnet run --project "EdFi.Ods.WebApi"
+}
+Start-Process powershell -WorkingDirectory $PSScriptRoot -ArgumentList {
+    &dotnet run --project "EdFi.Ods.SwaggerUI"
+}
+Start-Process powershell -WorkingDirectory $PSScriptRoot -ArgumentList {
+    &dotnet run --project "EdFi.Ods.SandboxAdmin"
+}
+
+Write-Output "WebAPI --> http://localhost:54746"
+Write-Output "SwaggerUI --> http://localhost:56641"
+Write-OUtput "Sandbox Admin --> http://localhost:38928"
