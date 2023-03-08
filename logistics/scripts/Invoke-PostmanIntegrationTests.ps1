4bf34e1b41646d893855b9a52d96172a60aab2ff
m.semalaiappan <m.semalaiappan@gmail.com>
2023-03-07 23:36:11 -0600

    Install-NpmPackages cleaned up

--- logistics/scripts/Invoke-PostmanIntegrationTests.ps1
+++ logistics/scripts/Invoke-PostmanIntegrationTests.ps1
@@ -75,11 +75,9 @@ function Install-NpmPackages {
     $does_corresponding_package_exist = npm list -g --depth=0 |Out-String -Stream | Select-String -Pattern $package.name -SimpleMatch -Quiet
     if (!$does_corresponding_package_exist -eq $true) {
       if (Get-IsWindows) {
-        #npm install -g "$($package.name)@$($package.requiredVersion)" --verbose --cache '%AppData%\npm-cache' --prefer-offline
         npm install -g "$($package.name)@$($package.requiredVersion)" --verbose
       }
       else {
-        #sudo npm install -g "$($package.name)@$($package.requiredVersion)" --verbose --cache '~/.npm/_cacache' --prefer-offline
         sudo npm install -g "$($package.name)@$($package.requiredVersion)" --verbose  
       }
     }
