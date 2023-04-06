35347d70ed8c4ec89cc4fe81a85551e6818ccbce
m.semalaiappan <m.semalaiappan@gmail.com>
2023-04-06 08:50:06 -0500

    SqlServer  Install module updated to specific version 21.1.18256

--- logistics/scripts/modules/database/database-management.psm1
+++ logistics/scripts/modules/database/database-management.psm1
@@ -23,7 +23,7 @@ function Use-SqlServerModule {
         }
 
         Write-Host "Installing SqlServer Module"
-        Install-Module -Name SqlServer -MinimumVersion "21.1.18068" -Scope CurrentUser -Force -AllowClobber | Out-Null
+        Install-Module -Name SqlServer -MinimumVersion "21.1.18256" -Scope CurrentUser -Force -AllowClobber | Out-Null
         Import-Module -Force -Scope Global SqlServer
     }
 }
