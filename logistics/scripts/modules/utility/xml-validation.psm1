af79d3c4ef355fe969530adf96d0325f5e7c3faa
m.semalaiappan <m.semalaiappan@gmail.com>
2023-06-01 17:43:28 -0500

    Reverting function Get-XmlRoot code changes

--- logistics/scripts/modules/utility/xml-validation.psm1
+++ logistics/scripts/modules/utility/xml-validation.psm1
@@ -16,23 +16,11 @@ function Get-XmlRoot {
         $streamReader = New-Object System.IO.StreamReader $source
         $xmlReader = [System.Xml.XmlReader]::Create($streamReader, (New-Object "System.Xml.XmlReaderSettings"))
         $xmlReader.MoveToContent() | Out-Null
-        $namespace = $xmlReader.NamespaceURI
-        $schema = ($xmlReader.GetAttribute("xsi:schemaLocation") -replace $xmlReader.NamespaceURI, "").Trim()
-        
-        $elementName = ""
-        while ($xmlReader.Read()) {
-            if ($xmlReader.NodeType -eq [System.Xml.XmlNodeType]::Element ) {
-                $elementname = $xmlReader.Name
-                break
-            }
-        }
-
         $result = @{
-            name = $elementName
-            namespace = $namespace
-            schema = $schema
+            name = $xmlReader.Name
+            namespace = $xmlReader.NamespaceURI
+            schema = ($xmlReader.GetAttribute("xsi:schemaLocation") -replace $xmlReader.NamespaceURI, "").Trim()
         }
-        
         return $result
     }
     finally {
