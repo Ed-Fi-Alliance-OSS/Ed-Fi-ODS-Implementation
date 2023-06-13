48ababbcf3290351eae0f323094af76ceb5f9f74
Stephen A. Fuqua <stephen@safnet.com>
2023-06-13 20:23:43 +0000

    Script for bootstrapping a local environment

---
+++ docker/bootstrap-client.sql
@@ -0,0 +1,11 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+insert into dbo.Vendors (VendorName) values ('Bootstrap Vendor');
+insert into dbo.VendorNamespacePrefixes (NamespacePrefix, Vendor_VendorId) select 'uri://ed-fi.org', VendorId from dbo.Vendors;
+insert into dbo.Applications (ApplicationName, OperationalContextUri, Vendor_VendorId, ClaimSetName) select 'Bootstrap Application', 'uri://ed-fi.org', VendorId, 'Ed-Fi Sandbox' from dbo.Vendors;
+insert into dbo.ApplicationEducationOrganizations (EducationOrganizationId, Application_ApplicationId) select 255901001, ApplicationId from dbo.Applications where ApplicationName = 'Bootstrap Application';
+insert into dbo.ApiClients (Key, Secret, Name, IsApproved, UseSandbox, SandboxType, SecretIsHashed, Application_ApplicationId) select 'populatedSandbox', 'populatedSandboxSecret', 'testing', true, false, 0, false, ApplicationId from dbo.Applications;
+insert into dbo.ApiClientApplicationEducationOrganizations (ApiClient_ApiClientId, ApplicationEdOrg_ApplicationEdOrgId ) select ApiClients.ApiClientId, ApplicationEducationOrganizations.ApplicationEducationOrganizationId from dbo.ApiClients cross join dbo.Applications inner join dbo.ApplicationEducationOrganizations on Applications.ApplicationId = ApplicationEducationOrganizations.Application_ApplicationId where ApiClients.Name = 'testing' and Applications.ApplicationName = 'Bootstrap Application'
---
+++ docker/bootstrap.ps1
@@ -0,0 +1,32 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
+# This script will startup a local ODS/API environment using local images built
+# from NuGet packages. This process includes provisioning of a self-signed
+# development certificate and of an initial key and secret with the "Ed-Fi
+# Sandbox" claimset.
+
+if (-not (Test-Path ./gateway/ssl/server.crt)) {
+    Push-Location gateway
+    ./generate-cert.ps1
+    Pop-Location
+}
+
+docker compose -f .\local-docker-compose.yml build `
+ --build-arg ADMIN_VERSION=7.0.98 `
+ --build-arg SECURITY_VERSION=7.0.78 `
+ --build-arg CORE_VERSION=7.0.656 `
+ --build-arg TPDM_VERSION=7.0.573 `
+ --build-arg API_VERSION=7.0.1596 `
+ --build-arg SWAGGER_VERSION=7.0.1994
+
+docker compose  -f .\local-docker-compose.yml up -d
+
+Start-Sleep -Seconds 10
+
+# Create bootstrapped key and secret: populatedSandbox, populatedSandboxSecret
+docker cp ./bootstrap-client.sql ed-fi-db-admin:/tmp/bootstrap-client.sql
+docker exec -i ed-fi-db-admin sh -c "psql -U postgres -d EdFi_Admin -f /tmp/bootstrap-client.sql"
+docker exec -i ed-fi-db-admin sh -c "rm /tmp/bootstrap-client.sql"
