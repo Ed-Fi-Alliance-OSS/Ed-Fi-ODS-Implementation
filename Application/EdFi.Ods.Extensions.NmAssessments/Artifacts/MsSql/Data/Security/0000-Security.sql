81b897088fef2f3ac015d8524e2e5f7ba8e0d78b
Jon Hickam <jon@redglobeinc.com>
2023-06-20 11:34:05 -0600

    Claim sets for assessment extension

---
+++ Application/EdFi.Ods.Extensions.NmAssessments/Artifacts/MsSql/Data/Security/0000-Security.sql
@@ -0,0 +1,34 @@
+-- Setting the application to Ed-Fi ODS API
+DECLARE @ApplicationId INT
+SELECT @ApplicationId = ApplicationId FROM [dbo].[Applications] WHERE ApplicationName = 'Ed-Fi ODS API';
+
+-- Getting the Claim Ids that we will use.  
+DECLARE @RelationshipBasedDataClaimId INT
+SELECT @RelationshipBasedDataClaimId = ResourceClaimId FROM [dbo].[ResourceClaims] WHERE ResourceName = 'relationshipBasedData';
+
+DECLARE @PrimaryRelationshipsClaimId INT
+SELECT @PrimaryRelationshipsClaimId = ResourceClaimId FROM [dbo].[ResourceClaims] WHERE ResourceName = 'primaryRelationships';
+
+DECLARE @DescriptorsClaimId INT
+SELECT @DescriptorsClaimId = ResourceClaimId FROM [dbo].[ResourceClaims] WHERE ResourceName = 'systemDescriptors';
+
+-- *** Extension Claims - Use this section to add NMPED specific additions to the model ***
+-- *** These should include any MetaEd defined extensions that are not "Additions" to and Ed-Fi core model element ***
+
+INSERT INTO [dbo].[ResourceClaims] ([DisplayName], [ResourceName] 
+								   ,[ClaimName] 
+								   ,[ParentResourceClaimId], [Application_ApplicationId])
+VALUES ('NmStudentAssessment', 'NmStudentAssessment'
+        ,'http://ed-fi.org/ods/identity/claims/nmped/localEducationAgencyTransportation'
+        ,@RelationshipBasedDataClaimId, @ApplicationId);
+
+-- *** Descriptors Security -- Adding these under the inherited permissions from System Descriptors ***
+-- *** These inserts should match the MetaEd entries under NMPEDMEtaED.Decriptor ***
+
+
+INSERT INTO ResourceClaims (DisplayName, ResourceName, ClaimName, ParentResourceClaimId, Application_ApplicationId) VALUES
+('standardAchievedCodeDescriptor','standardAchievedCodeDescriptor','http://ed-fi.org/ods/identity/claims/nmped/standardAchievedCodeDescriptor',@DescriptorsClaimId,@ApplicationId),
+('itemDescriptionCodeDescriptor','itemDescriptionCodeDescriptor','http://ed-fi.org/ods/identity/claims/nmped/itemDescriptionCodeDescriptor',@DescriptorsClaimId,@ApplicationId),
+('testDescriptionDescriptor','testDescriptionDescriptor','http://ed-fi.org/ods/identity/claims/nmped/testDescriptionDescriptor',@DescriptorsClaimId,@ApplicationId);
+
+
