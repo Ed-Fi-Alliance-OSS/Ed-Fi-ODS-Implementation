c7aaf4920fecfa77dac8ec4141f85de454b11f57
Matthew Jackson <mjackson@unicon.net>
2023-07-10 12:10:39 -0500

    Fix logic on settings modification

--- logistics/scripts/modules/settings/settings-management.psm1
+++ logistics/scripts/modules/settings/settings-management.psm1
@@ -542,7 +542,7 @@ function Update-DefaultDatabaseTemplate([hashtable] $Settings = @{ }) {
 }
 
 function Add-OdsConnectionStringEncryptionKey([hashtable] $Settings = @{ }, [string] $ProjectName) {
-    if ($ProjectName -ne ((Get-ProjectTypes).WebApi)) { return $Settings }
+    if (-not $ProjectName.Contains("Ods")) { return $Settings }
 
     if([string]::IsNullOrWhiteSpace($settings.ApiSettings.OdsConnectionStringAESKey)) {
         $Settings.ApiSettings.OdsConnectionStringAESKey = New-AESKey
@@ -680,7 +680,7 @@ function New-DevelopmentAppSettings([hashtable] $Settings = @{ }) {
         
         $newDevelopmentSettings = Remove-ODSConnectionString $newDevelopmentSettings $project
 
-        $newDevelopmentSettings = Add-OdsConnectionStringEncryptionKey $newDevelopmentSettings @project
+        $newDevelopmentSettings = Add-OdsConnectionStringEncryptionKey $newDevelopmentSettings $Project
 
         if ($Settings.InstallType -eq 'MultiTenant')
         {
