38b06a865d3435879f0956dba71332067c60841f
Matthew Jackson <mjackson@unicon.net>
2023-07-13 16:34:35 -0500

    Fix setting name

--- logistics/scripts/modules/settings/settings-management.psm1
+++ logistics/scripts/modules/settings/settings-management.psm1
@@ -559,8 +559,8 @@ function Update-DefaultDatabaseTemplate([hashtable] $Settings = @{ }) {
 function Add-OdsConnectionStringEncryptionKey([hashtable] $Settings = @{ }, [string] $ProjectName, [string] $AESKey) {
     if ((-not $ProjectName.Contains("Ods")) -or ($ProjectName.Contains("Swagger"))) { return $Settings }
 
-    if([string]::IsNullOrWhiteSpace($settings.ApiSettings.OdsConnectionStringAESKey)) {
-        $Settings.ApiSettings.OdsConnectionStringAESKey = $AESKey
+    if([string]::IsNullOrWhiteSpace($settings.ApiSettings.OdsConnectionStringEncryptionKey)) {
+        $Settings.ApiSettings.OdsConnectionStringEncryptionKey = $AESKey
     }
 
     return $Settings
