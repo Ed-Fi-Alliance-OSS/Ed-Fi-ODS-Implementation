563949d0b124de58a81f94b448f780a4ed53b5dc
Jesus Flores <jesus@simpat.tech>
2023-09-05 16:47:47 -0600

    Fixed tag

--- Docker/build-images.ps1
+++ Docker/build-images.ps1
@@ -1,4 +1,4 @@
-# SPDX-License-Identifier: Apache-2.0
+ # SPDX-License-Identifier: Apache-2.0
 # Licensed to the Ed-Fi Alliance under one or more agreements.
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
@@ -144,7 +144,7 @@ function Invoke-Build {
         $mssql = "-mssql"
     }
 
-    $stdVer = "-$env:CURRENT_STANDARD_VERSION"
+    $stdVer = "-$StandardVersion"
     if ($env:CURRENT_STANDARD_VERSION -eq $StandardVersion) {
         $stdVer = ""
     }
