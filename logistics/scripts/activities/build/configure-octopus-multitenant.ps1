8b3f0368871a986047225fccbc2a7820ebf7447d
Matthew Jackson <mjackson@unicon.net>
2023-09-14 15:36:11 -0500

    Formatting

--- logistics/scripts/activities/build/configure-octopus-multitenant.ps1
+++ logistics/scripts/activities/build/configure-octopus-multitenant.ps1
@@ -4,4 +4,5 @@
 Import-Module -Force -Scope Global './configure-tenant.psm1'
 
 Invoke-ConfigureOctopusTenant -TenantIdentifier "Tenant 1" -TenantPopulatedSandboxKey $OctopusParameters["PrepopulatedKeyTenant1"] -TenantPopulatedSandboxSecret $OctopusParameters["PrepopulatedSecretTenant1"] -TenantsJson $OctopusParameters["Tenants"] -ConnectionString $OctopusParameters["ConnectionStrings:EdFi_Ods"] -TenantVendorName "tenant1.org"
+
 Invoke-ConfigureOctopusTenant -TenantIdentifier "Tenant 2" -TenantPopulatedSandboxKey $OctopusParameters["PrepopulatedKeyTenant2"] -TenantPopulatedSandboxSecret $OctopusParameters["PrepopulatedSecretTenant2"] -TenantsJson $OctopusParameters["Tenants"] -ConnectionString $OctopusParameters["ConnectionStrings:EdFi_Ods"] -TenantVendorName "tenant2.org"
