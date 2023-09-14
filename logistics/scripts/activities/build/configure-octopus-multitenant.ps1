ffc422559435c5de212c692f0167b15679fef69e
Matthew Jackson <mjackson@unicon.net>
2023-09-14 16:35:59 -0500

    Fix path in script

--- logistics/scripts/activities/build/configure-octopus-multitenant.ps1
+++ logistics/scripts/activities/build/configure-octopus-multitenant.ps1
@@ -1,7 +1,7 @@
 # This script will inject the ODS connection strings and the populatedSandbox 
 # keys and secrets into the Admin database for two Tenants
 
-Import-Module -Force -Scope Global './configure-tenant.psm1'
+Import-Module -Force -Scope Global '#{Octopus.Action[Deploy Tenant 1 & Tenant 2 Databases].Output.Package.InstallationDirectoryPath}/Ed-Fi-ODS-Implementation/logistics/scripts/activities/build/configure-tenant.psm1'
 
 Invoke-ConfigureOctopusTenant -TenantIdentifier "Tenant 1" -TenantPopulatedSandboxKey $OctopusParameters["PrepopulatedKeyTenant1"] -TenantPopulatedSandboxSecret $OctopusParameters["PrepopulatedSecretTenant1"] -TenantsJson $OctopusParameters["Tenants"] -ConnectionString $OctopusParameters["ConnectionStrings:EdFi_Ods"] -TenantVendorName "tenant1.org"
 
