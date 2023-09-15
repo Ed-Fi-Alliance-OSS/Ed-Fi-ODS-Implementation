d931ecc4082206c84757f7a38826893f5f60ee8b
Matthew Jackson <mjackson@unicon.net>
2023-09-15 07:45:55 -0500

    Fix tenant names

--- logistics/scripts/activities/build/configure-octopus-multitenant.ps1
+++ logistics/scripts/activities/build/configure-octopus-multitenant.ps1
@@ -161,6 +161,6 @@ function Invoke-ConfigureOctopusTenant {
     Invoke-SqlCmd -ConnectionString $tenantAdminDatabaseCS -Query $sql 
 }
 
-Invoke-ConfigureOctopusTenant -TenantIdentifier "Tenant 1" -TenantPopulatedSandboxKey $OctopusParameters["PrepopulatedKeyTenant1"] -TenantPopulatedSandboxSecret $OctopusParameters["PrepopulatedSecretTenant1"] -TenantsJson $OctopusParameters["Tenants"] -ConnectionString $OctopusParameters["ConnectionStrings:EdFi_Ods"] -TenantVendorName "tenant1.org"
+Invoke-ConfigureOctopusTenant -TenantIdentifier "Tenant1" -TenantPopulatedSandboxKey $OctopusParameters["PrepopulatedKeyTenant1"] -TenantPopulatedSandboxSecret $OctopusParameters["PrepopulatedSecretTenant1"] -TenantsJson $OctopusParameters["Tenants"] -ConnectionString $OctopusParameters["ConnectionStrings:EdFi_Ods"] -TenantVendorName "tenant1.org"
 
-Invoke-ConfigureOctopusTenant -TenantIdentifier "Tenant 2" -TenantPopulatedSandboxKey $OctopusParameters["PrepopulatedKeyTenant2"] -TenantPopulatedSandboxSecret $OctopusParameters["PrepopulatedSecretTenant2"] -TenantsJson $OctopusParameters["Tenants"] -ConnectionString $OctopusParameters["ConnectionStrings:EdFi_Ods"] -TenantVendorName "tenant2.org"
+Invoke-ConfigureOctopusTenant -TenantIdentifier "Tenant2" -TenantPopulatedSandboxKey $OctopusParameters["PrepopulatedKeyTenant2"] -TenantPopulatedSandboxSecret $OctopusParameters["PrepopulatedSecretTenant2"] -TenantsJson $OctopusParameters["Tenants"] -ConnectionString $OctopusParameters["ConnectionStrings:EdFi_Ods"] -TenantVendorName "tenant2.org"
