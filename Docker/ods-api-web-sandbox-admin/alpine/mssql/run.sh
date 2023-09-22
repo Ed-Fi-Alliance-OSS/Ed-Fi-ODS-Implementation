7e245f13349c5c7de87f407249f73de319fc68b2
Jesus Flores <43448209+simpat-jesus@users.noreply.github.com>
2023-09-22 15:44:07 -0600

    [ODS-6014] msql connection doesn't work with published docker images (#807)
    * Update mssql api image to include missing env variables
    
    * Update mssql sandbox admin image to include missing env variables
    
    * Update action to enable manual publish to docker hub
    
    * Update image name in readme

--- Docker/ods-api-web-sandbox-admin/alpine/mssql/run.sh
+++ Docker/ods-api-web-sandbox-admin/alpine/mssql/run.sh
@@ -7,15 +7,38 @@
 set -e
 set +x
 
+if [[ -n "$SQLSERVER_USER" && -n "$SQLSERVER_PASSWORD" ]]; then
+  echo "Overriding SQLSERVER_ADMIN_USER..."
+  export SQLSERVER_ADMIN_USER=$SQLSERVER_USER
+
+  echo "Overriding SQLSERVER_ADMIN_PASSWORD..."
+  export SQLSERVER_ADMIN_PASSWORD=$SQLSERVER_PASSWORD
+
+  echo "Overriding SQLSERVER_ODS_USER..."
+  export SQLSERVER_ODS_USER=$SQLSERVER_USER
+
+  echo "Overriding SQLSERVER_ODS_PASSWORD..."
+  export SQLSERVER_ODS_PASSWORD=$SQLSERVER_PASSWORD
+fi
+
+# Validate Admin Database User and Password
+if [[ -z "$SQLSERVER_ADMIN_USER" || -z "$SQLSERVER_ADMIN_PASSWORD" ]]; then
+  echo "Admin database User and Password are required."
+  exit 1
+fi
+
+# Validate ODS Database User and Password
+if [[ -z "$SQLSERVER_ODS_USER" && -z "$SQLSERVER_ODS_PASSWORD" ]]; then
+  echo "ODS database User and Password are required."
+  exit 1
+fi
+
+if [[ "$ENCRYPT_CONNECTION" == true ]]; then
+    export ENCRYPT_CONNECTION=""
+elif [[ "$ENCRYPT_CONNECTION" != true ]]; then
+    export ENCRYPT_CONNECTION="Encrypt=false;"
+fi
+
 envsubst < /app/appsettings.template.json > /app/appsettings.json
-
-# until PGPASSWORD=$POSTGRES_PASSWORD psql -h $ODS_POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -c '\q';
-# do
-#   >&2 echo "Postgres is unavailable - sleeping"
-#   sleep 10
-# done
-
-# >&2 echo "Postgres is up - executing command"
-# exec $cmd
 
 dotnet EdFi.Ods.Sandbox.Admin.dll
