84ae9d8ff98550d8c6f785eb05c58de996d36b35
Matthew Jackson <mjackson@unicon.net>
2023-10-16 17:29:05 -0500

    Update for test harness configuration

--- Application/EdFi.Ods.Api.IntegrationTestHarness/UpdateAdminDatabaseTask.cs
+++ Application/EdFi.Ods.Api.IntegrationTestHarness/UpdateAdminDatabaseTask.cs
@@ -197,7 +197,7 @@ namespace EdFi.Ods.Api.IntegrationTestHarness
             _clientAppRepo.CreateProfilesWithProfileDefinition(profiles);
 
             foreach (var vendor in _testHarnessConfiguration.Vendors.Where(x =>
-                string.IsNullOrEmpty(tenantIdentifier) ||
+                string.IsNullOrEmpty(x.TenantIdentifier) ||
                 x.TenantIdentifier.Equals(tenantIdentifier, StringComparison.InvariantCultureIgnoreCase)))
             {
                 var user = _clientAppRepo.GetUser(vendor.Email) ??
