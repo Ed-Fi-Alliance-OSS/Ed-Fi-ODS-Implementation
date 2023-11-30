408ad1aa565541cb5f2de62ce845d8b194a5e9a9
Jesus Flores <jesus@simpat.tech>
2023-11-30 17:00:01 -0600

    Reverted changes

--- logistics/scripts/modules/database/postgres-database-management.psm1
+++ logistics/scripts/modules/database/postgres-database-management.psm1
@@ -91,7 +91,7 @@ function Invoke-PsqlCommand {
 
     if ($userName) { $params += @("--username", $userName) }
 
-    $commands | ForEach-Object { $params += @("--command", "$_") }
+    $commands | ForEach-Object { $params += @("--command", "`"$_`"") }
 
     $psql = Get-PSQLPath
     Write-Host -ForegroundColor Magenta "& $psql $params"
@@ -258,7 +258,9 @@ function Remove-PostgreSQLDatabase {
             # This suppresses any non-error messages from outputting through stderr and causing TeamCity build failures
             # while still allowing any errors to fail the build.
             "SET client_min_messages TO ERROR;"
-            "DROP DATABASE IF EXISTS $databaseName;"
+            # note: the backslash is required for passing quotation mark into psql command line, and
+            # the backtick is required for escaping the quotation mark in PowerShell.
+            "DROP DATABASE IF EXISTS \`"$databaseName\`";"
         )
     }
 
