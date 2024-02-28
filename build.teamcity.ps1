aa6eaa801f96e839d2b0a224d25521187c5c91e5
Vinaya Mayya <vinaya.mayya@ed-fi.org>
2024-02-27 23:11:17 -0600

    GitHub Actions for 5x

--- build.teamcity.ps1
+++ build.teamcity.ps1
@@ -14,18 +14,21 @@ Write-Host
 
 # Build and Test
 $params = @{
-    InstallType   = Get-ValueOrDefault                    $teamcityParameters['odsapi.build.installType']    'Sandbox'
-    OdsTokens     = Get-ValueOrDefault (ConvertTo-Array   $teamcityParameters['odsapi.build.odsTokens'])     @()
-    Engine        = Get-ValueOrDefault                    $teamcityParameters['odsapi.build.engine']         'SQLServer'
-    NoCodeGen     = Get-ValueOrDefault (ConvertTo-Boolean $teamcityParameters['odsapi.build.noCodeGen'])     $false
-    NoRebuild     = Get-ValueOrDefault (ConvertTo-Boolean $teamcityParameters['odsapi.build.noRebuild'])     $false
-    NoDeploy      = Get-ValueOrDefault (ConvertTo-Boolean $teamcityParameters['odsapi.build.noDeploy'])      $false
-    RunPester     = Get-ValueOrDefault (ConvertTo-Boolean $teamcityParameters['odsapi.build.runPester'])     $true
-    RunDotnetTest = Get-ValueOrDefault (ConvertTo-Boolean $teamcityParameters['odsapi.build.runDotnetTest']) $true
-    RunPostman    = Get-ValueOrDefault (ConvertTo-Boolean $teamcityParameters['odsapi.build.runPostman'])    $true
-    RunSmokeTest  = Get-ValueOrDefault (ConvertTo-Boolean $teamcityParameters['odsapi.build.runSmokeTest'])  $true
-    UsePlugins    = Get-ValueOrDefault (ConvertTo-Boolean $teamcityParameters['odsapi.build.usePlugins'])    $false
-    RunSdkGen     = Get-ValueOrDefault (ConvertTo-Boolean $teamcityParameters['odsapi.build.runSdkGen'])     $false
+    InstallType            = Get-ValueOrDefault                    $teamcityParameters['odsapi.build.installType']             'Sandbox'
+    OdsTokens              = Get-ValueOrDefault (ConvertTo-Array   $teamcityParameters['odsapi.build.odsTokens'])              @()
+    Engine                 = Get-ValueOrDefault                    $teamcityParameters['odsapi.build.engine']                  'SQLServer'
+    NoCodeGen              = Get-ValueOrDefault (ConvertTo-Boolean $teamcityParameters['odsapi.build.noCodeGen'])              $false
+    NoRebuild              = Get-ValueOrDefault (ConvertTo-Boolean $teamcityParameters['odsapi.build.noRebuild'])              $false
+    NoDeploy               = Get-ValueOrDefault (ConvertTo-Boolean $teamcityParameters['odsapi.build.noDeploy'])               $false
+    RunPester              = Get-ValueOrDefault (ConvertTo-Boolean $teamcityParameters['odsapi.build.runPester'])              $true
+    RunDotnetTest          = Get-ValueOrDefault (ConvertTo-Boolean $teamcityParameters['odsapi.build.runDotnetTest'])          $true
+    RunPostman             = Get-ValueOrDefault (ConvertTo-Boolean $teamcityParameters['odsapi.build.runPostman'])             $true
+    RunSmokeTest           = Get-ValueOrDefault (ConvertTo-Boolean $teamcityParameters['odsapi.build.runSmokeTest'])           $true
+    UsePlugins             = Get-ValueOrDefault (ConvertTo-Boolean $teamcityParameters['odsapi.build.usePlugins'])             $false
+    RunSdkGen              = Get-ValueOrDefault (ConvertTo-Boolean $teamcityParameters['odsapi.build.runSdkGen'])              $true
+    GenerateApiSdkPackage  = Get-ValueOrDefault (ConvertTo-Boolean $teamcityParameters['odsapi.build.generateApiSdkPackage'])  $false
+    GenerateTestSdkPackage = Get-ValueOrDefault (ConvertTo-Boolean $teamcityParameters['odsapi.build.generateTestSdkPackage']) $false
+    PackageVersion         = Get-ValueOrDefault                    $teamcityParameters['version']                             '0.0.0'
 }
 
 Invoke-Task "Remove-EdFiDatabases" { Remove-EdFiDatabases -Force -Engine $params.Engine }
