3095df9b56e2d645d02376e6322bd206fc81325a
Matthew Jackson <mjackson@unicon.net>
2024-02-28 14:10:34 -0600

    Move Hangfire jobs to Quartz

--- Application/EdFi.Ods.SandboxAdmin/Services/BackgroundJobService.cs
+++ Application/EdFi.Ods.SandboxAdmin/Services/BackgroundJobService.cs
@@ -1,46 +1,131 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading.Tasks;
+using EdFi.Ods.Common.Configuration;
 using EdFi.Ods.Sandbox.Admin.Initialization;
-using Hangfire;
+using log4net;
 using Microsoft.Extensions.Options;
+using Quartz;
+using Quartz.Impl;
 
 namespace EdFi.Ods.Sandbox.Admin.Services
 {
     public class BackgroundJobService : IBackgroundJobService
     {
+        private readonly ILog _log = LogManager.GetLogger(typeof(BackgroundJobService));
         private readonly IInitializationEngine _engine;
         private readonly Dictionary<string, UserOptions> _users;
+        private readonly ApiSettings _apiSettings;
 
-        public BackgroundJobService(IInitializationEngine engine, IOptions<Dictionary<string, UserOptions>> users)
+        public BackgroundJobService(IInitializationEngine engine, IOptions<Dictionary<string, UserOptions>> users,
+            ApiSettings apiSettings)
         {
             _engine = engine;
             _users = users.Value;
+            _apiSettings = apiSettings;
         }
 
         /// <summary>
         /// Create background jobs to populate users and sandboxes
-        /// This class separates the initialization engine from HangFire
+        /// This class separates the initialization engine from Quartz
         /// </summary>
-        public void Configure()
+        public async void Configure()
         {
-            // initial creation of roles, users, and sandboxes at server startup 
-            var id1 = BackgroundJob.Enqueue(() => _engine.CreateIdentityRoles());
-            BackgroundJob.ContinueJobWith(id1, () => _engine.CreateIdentityUsers());
-
-            // add vendors and sandboxes
-            var id2 = BackgroundJob.Enqueue(() => _engine.CreateVendors());
-            var id3 = BackgroundJob.ContinueJobWith(id2, () => _engine.CreateSandboxes());
-
+            StdSchedulerFactory factory = new StdSchedulerFactory();
+            IScheduler scheduler = await factory.GetScheduler();
+            
+            await scheduler.Start();
+            
+            await CreateAndScheduleNewJob<CreateIdentityRolesJob>("createIdentityRoles", "setupAdminDatabase", scheduler);
+            await CreateAndScheduleNewJob<CreateIdentityUsersJob>("createIdentityUsers", "setupAdminDatabase", scheduler);
+            await CreateAndScheduleNewJob<CreateVendorsJob>("createVendors", "setupAdminDatabase", scheduler);
+            
+            await CreateAndScheduleNewJob<CreateSandboxesJob>("createSandboxes", "setupAdminDatabase", scheduler);
+            
             foreach (var user in _users)
             {
                 // refresh existing sandboxes periodically
                 if (user.Value.Sandboxes.Any(s => s.Value.Refresh))
                 {
                     // Change the recurrence to suit your needs using Cron functions or a unix CRON expressions (i.e. "* */6 * * *" = every 6 hours)
-                    RecurringJob.AddOrUpdate("RebuildSandboxes", () => _engine.RebuildSandboxes(), Cron.Daily(), TimeZoneInfo.Local);
+                    await CreateAndScheduleNewJob<CreateSandboxesJob>("createSandboxes", "setupAdminDatabase", scheduler, "* */24 * * *");
                 }
             }
+        }
+
+        private async Task<IJobDetail> CreateAndScheduleNewJob<T>(string idName, string idGroup, IScheduler scheduler, string cronExpression = null)
+            where T : IJob
+        {
+            var newJob = JobBuilder.Create<T>().WithIdentity(idName, idGroup).Build();
+            newJob.JobDataMap.Put("engine", _engine);
+            ITrigger newTrigger;
+            
+            // If no cron expression is provided, then use a simple schedule that runs once
+            if (string.IsNullOrEmpty(cronExpression))
+            {
+                newTrigger = TriggerBuilder.Create()
+                    .WithIdentity(idName, idGroup)
+                    .WithSimpleSchedule()
+                    .StartNow()
+                    .Build();
+            }
+            else
+            {
+                newTrigger = TriggerBuilder.Create()
+                    .WithIdentity(idName, idGroup)
+                    .WithCronSchedule(cronExpression)
+                    .StartNow()
+                    .Build();
+            }
+
+            await scheduler.ScheduleJob(newJob, newTrigger);
+            return newJob;
+        }
+    }
+
+    [DisallowConcurrentExecution]
+    public class CreateIdentityRolesJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await ((IInitializationEngine)context.MergedJobDataMap["engine"]).CreateIdentityRoles();
+        }
+    }
+
+    [DisallowConcurrentExecution]
+    public class CreateIdentityUsersJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await ((IInitializationEngine)context.MergedJobDataMap["engine"]).CreateIdentityUsers();
+        }
+    }
+
+    [DisallowConcurrentExecution]
+    public class CreateVendorsJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            ((IInitializationEngine)context.MergedJobDataMap["engine"]).CreateVendors();
+        }
+    }
+
+    [DisallowConcurrentExecution]
+    public class RebuildSandboxesJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            ((IInitializationEngine)context.MergedJobDataMap["engine"]).CreateSandboxes();
+        }
+    }
+
+    [DisallowConcurrentExecution]
+    public class CreateSandboxesJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            ((IInitializationEngine)context.MergedJobDataMap["engine"]).CreateSandboxes();
         }
     }
 }
