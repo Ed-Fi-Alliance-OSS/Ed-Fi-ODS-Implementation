fb54170383e3d5f836ba9973f49c9262a38e868f
Axel Marquez <axel.marqz@gmail.com>
2024-07-23 16:26:20 -0600

    Dummy edit

--- build.github.ps1
+++ build.github.ps1
@@ -2,6 +2,9 @@
 # Licensed to the Ed-Fi Alliance under one or more agreements.
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
+
+# Dummy edit
+
 [CmdLetBinding()]
 param(
     [ValidateSet('Sandbox', 'SingleTenant', 'MultiTenant')]
