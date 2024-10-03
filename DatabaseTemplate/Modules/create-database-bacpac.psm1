933708eda7475e4d2990994fef5d2b54457d4127
m.semalaiappan <m.semalaiappan@gmail.com>
2024-10-03 12:25:04 -0500

    Passing Connection String sqlpackage

--- DatabaseTemplate/Modules/create-database-bacpac.psm1
+++ DatabaseTemplate/Modules/create-database-bacpac.psm1
@@ -88,7 +88,7 @@ function Export-BacPac {
     $params += "/a:export"
     $params += "/sdn:" + $database
     $params += "/tf:" + $artifactOutput
-    $params += "/ssn:" + $server
+    $params += "/cs:" + $existingCSB
     $params += "/sec:False" # Source Encrypt Connection
 
     Write-Host -ForegroundColor Magenta $executableName $params
