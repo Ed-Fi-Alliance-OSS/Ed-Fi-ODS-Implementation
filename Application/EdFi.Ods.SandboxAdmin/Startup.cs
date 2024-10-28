c6c9ed71727a3cdda9e8d6816138cdcb46bc2ebe
Jesus Flores <jesus@simpat.tech>
2024-10-28 14:59:17 -0600

    Fix warnings

--- Application/EdFi.Ods.SandboxAdmin/Startup.cs
+++ Application/EdFi.Ods.SandboxAdmin/Startup.cs
@@ -99,7 +99,7 @@ namespace EdFi.Ods.SandboxAdmin
                     }
                     else
                     {
-                        config.UsePostgreSqlStorage(Configuration.GetConnectionString("EdFi_Admin"));
+                        config.UsePostgreSqlStorage(options => options.UseNpgsqlConnection(Configuration.GetConnectionString("EdFi_Admin")));
                     }
                 });
 
