0c615d25ca541b14d2885156948365abf8ac7a4e
m.semalaiappan <m.semalaiappan@gmail.com>
2024-12-04 19:21:57 -0600

    Review comments CodeFix

--- Docker/ods-api-web-api/alpine/pgsql/run.sh
+++ Docker/ods-api-web-api/alpine/pgsql/run.sh
@@ -25,7 +25,7 @@ do
       PGHOST=$HOST \
       PGPORT=$POSTGRES_PORT \
       PGUSER=$POSTGRES_USER \
-      pg_isready -h "$HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER" -d "$POSTGRES_DB" > /dev/null
+      pg_isready -d "$POSTGRES_DB" > /dev/null
   do
     >&2 echo "Postgres '$HOST' is unavailable - sleeping"
     sleep 10
