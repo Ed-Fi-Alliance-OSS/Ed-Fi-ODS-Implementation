80269b208ccd1bf702aa2c16b1b6a44b4d490345
semalaiappan <34613894+semalaiappan@users.noreply.github.com>
2025-01-13 13:08:49 -0600

    Update StartupFactory.cs

--- Application/EdFi.Ods.WebApi/StartupFactory.cs
+++ Application/EdFi.Ods.WebApi/StartupFactory.cs
@@ -28,7 +28,7 @@ namespace EdFi.Ods.WebApi
             var startupType = AppDomain.CurrentDomain.GetAssemblies()
                 .Where(assembly => assembly.GetName().Name?.StartsWith("EdFi.") ?? false)
                 .SelectMany(assembly => assembly.GetTypes())
-                .Where(t => typeof(OdsStartupBase).IsAssignableFrom(t) && t.Name == startupClassName)
+                .Where(t => t.Name.EqualsIgnoreCase(startupClassName) && typeof(OdsStartupBase).IsAssignableFrom(t))                
                 .FirstOrDefault();
 
             if (startupType == null)
