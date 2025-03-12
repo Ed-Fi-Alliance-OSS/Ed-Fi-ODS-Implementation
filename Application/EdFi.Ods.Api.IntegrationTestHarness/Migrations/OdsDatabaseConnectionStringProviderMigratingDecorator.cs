bbca56090938288a9f4af46f143477d8ec8639d2
Matthew Jackson <mjackson@unicon.net>
2025-03-12 16:05:43 -0500

    Make test harness database migrations optional

--- Application/EdFi.Ods.Api.IntegrationTestHarness/Migrations/OdsDatabaseConnectionStringProviderMigratingDecorator.cs
+++ Application/EdFi.Ods.Api.IntegrationTestHarness/Migrations/OdsDatabaseConnectionStringProviderMigratingDecorator.cs
@@ -6,6 +6,7 @@
 using System.Collections.Concurrent;
 using System.Data.Common;
 using System.Reflection;
+using EdFi.Common.Configuration;
 using EdFi.Common.Database;
 using EdFi.Ods.Common.Caching;
 using EdFi.Ods.Common.Database;
@@ -21,15 +22,18 @@ public class OdsDatabaseConnectionStringProviderMigratingDecorator : IOdsDatabas
     private readonly ConcurrentDictionary<ulong, bool> _migratedConnections = new();
     private readonly IOdsDatabaseConnectionStringProvider _odsDatabaseConnectionStringProvider;
     private readonly DbProviderFactory _dbProviderFactory;
+    private readonly TestHarnessConfigurationProvider _testHarnessConfigurationProvider;
 
     public OdsDatabaseConnectionStringProviderMigratingDecorator(
         IDatabaseMigrationsApplicator databaseMigrationsApplicator,
         IOdsDatabaseConnectionStringProvider odsDatabaseConnectionStringProvider,
+        TestHarnessConfigurationProvider testHarnessConfigurationProvider,
         DbProviderFactory dbProviderFactory)
     {
         _databaseMigrationsApplicator = databaseMigrationsApplicator;
         _odsDatabaseConnectionStringProvider = odsDatabaseConnectionStringProvider;
         _dbProviderFactory = dbProviderFactory;
+        _testHarnessConfigurationProvider = testHarnessConfigurationProvider;
     }
 
     string IDatabaseConnectionStringProvider.GetConnectionString()
@@ -52,13 +56,21 @@ public class OdsDatabaseConnectionStringProviderMigratingDecorator : IOdsDatabas
                 standardVersionText = cmd.ExecuteScalar()?.ToString();
             }
 
+            if (!_testHarnessConfigurationProvider.GetTestHarnessConfiguration().ApplyOdsDatabaseMigrations)
+            {
+                return connectionString;
+            }
+
             if (!string.IsNullOrWhiteSpace(standardVersionText))
             {
                 string[] versionParts = standardVersionText.Split('.');
                 string major = versionParts[0];
                 string minor = versionParts[1];
-                string revision = (versionParts.Length > 2) ? versionParts[2] : "0";
-                
+
+                string revision = (versionParts.Length > 2)
+                    ? versionParts[2]
+                    : "0";
+
                 standardVersion = $"{major}.{minor}.{revision}";
                 _logger.Info($"Applying ODS migrations (Standard Version: {standardVersion})...");
             }
