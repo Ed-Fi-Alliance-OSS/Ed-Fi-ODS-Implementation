126d92489f544291353b1bbef9dda10a59c84630
semalaiappan <34613894+semalaiappan@users.noreply.github.com>
2025-09-23 11:42:35 -0500

    [ODS-4330] EdFi.Suite3.Installer.WebApi EdFi.Suite3.Installer.SwaggerUI EdFi.Suite3.Installer.SandboxAdmin Issue CodeFix (#1282)

--- logistics/scripts/modules/packaging/create-package.psm1
+++ logistics/scripts/modules/packaging/create-package.psm1
@@ -101,7 +101,6 @@ function Invoke-CreatePackage {
             PackageFile = (Get-ChildItem "$OutputDirectory/$packageId*.$Version.nupkg").FullName
             Source      = $Source
             ApiKey      = $ApiKey
-            Verbose     = $verbose
         }
         Publish-PrereleasePackage @parameters
     }
@@ -157,16 +156,36 @@ function New-Package {
     $parameters += "--configuration"
     $parameters += $BuildConfiguration
 
-    $nuspecProperties = "-p:NuspecProperties=""version=$($Version)"
+    $nuspecProperties = "-p:NuspecProperties=""version=$($Version)"""
+    $parameters += $nuspecProperties
+    
+    [xml]$xml = Get-Content -Path $PackageDefinitionFile
 
+    # Split "key=value" pairs
+    $Properties -split ';' | ForEach-Object {
+        if ($_ -match "^(?<key>[^=]+)=(?<value>.*)$") {
+            $key   = $matches['key']
+            $value = $matches['value']
 
-    foreach ($prop in $Properties) {
-        $nuspecProperties += ";$prop"
+            if ([string]::IsNullOrWhiteSpace($key)) { return }
+            if ([string]::IsNullOrWhiteSpace($value)) { 
+                Write-Host "Skipping $key (value is null or empty)"
+                return
+            }
+            # Check if node already exists
+            if ($xml.package.metadata.$key) {
+                $xml.package.metadata.$key = $value
+            }
+            else {
+                $node = $xml.CreateElement($key)
+                $node.InnerText = $value
+                $xml.package.metadata.AppendChild($node) | Out-Null
+            }
+        }
     }
 
-    $nuspecProperties += """"
+    $xml.Save($PackageDefinitionFile)
 
-    $parameters += $nuspecProperties
 
     if ($Verbose) {
         $parameters += "--verbosity"
@@ -214,10 +233,6 @@ function Publish-PrereleasePackage {
         "--api-key", $ApiKey
     )
 
-    if ($Verbose) {
-        $parameters += "--verbosity"
-        $parameters += "detailed"
-    }
 
     & dotnet nuget @parameters
 }
